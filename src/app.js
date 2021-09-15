e71149426b8d144fa6395d8796ac9492ce432b2b
teyz <contact@bastienrigaud.fr>
2021-09-15 15:53:14 +0200

    Fix: changes

--- src/app.js
+++ src/app.js
@@ -1,64 +1,80 @@
 const express = require("express");
 const crypto = require("crypto");
-const fetch = require('node-fetch');
-const tmi = require('tmi.js');
-const token = require('./token.js');
+const bodyParser = require('body-parser');
 
 require('dotenv').config();
-
-/** Twitch Bot  */
-
-const options = {
-    options: {
-        debug: true
-    },
-    connection: {
-        reconnect: true
-    },
-    identity: {
-        username: token.bot_username,
-        password: token.password_oauth,
-    },
-    channels: [token.channels]
-};
-
-const client = new tmi.Client(options);
-
-client.on('message', onMessageHandler);
-client.on('connected', onConnectedHandler);
-
-client.connect();
-
-// Called every time a message comes in
-function onMessageHandler(target, context, msg, self) {
-    if (self) { return; } // Ignore messages from the bot
-
-    // Remove whitespace from chat message
-    const commandName = msg.trim();
-
-    if (commandName === '!discord') {
-        client.say(target, `N'hésite pas à rejoindre le discord : https://discord.gg/QSqPzC9MGF`);
-    } else {
-        console.log(`* Unknown command ${commandName}`);
-    }
-
-    if (commandName === '!twitter') {
-        client.say(target, `N'hésite pas à me follow sur twitter : https://twitter.com/FrTeyz`);
-    } else {
-        console.log(`* Unknown command ${commandName}`);
-    }
-}
-
-// Called every time the bot connects to Twitch chat
-function onConnectedHandler(addr, port) {
-    console.log(`* Connected to ${addr}:${port}`);
-}
-
-/** Twitch Bot  */
+const alertsData = require('../assets/alerts.json');
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 const port = process.env.PORT || 2000;
 const secret = process.env.SECRET;
+let alertsEventsData = [];
+let events = [];
+
+
+/* Random Logics */
+
+const isWin = () => {
+    var d = Math.random();
+    if (d <= 0.004) {
+        return true;
+    }
+    return false;
+};
+
+/* Random Logics */
+
+/* Set Alert Logic */
+
+const setAlert = (newSubData, eventType) => {
+    if (eventType === "channel.subscribe")
+        sendEventsToAll(setAlertBasicSub());
+    else
+        setAlertSubWithMonth(newSubData);
+};
+
+const setAlertBasicSub = () => {
+    const isSubWin = isWin();
+    if (isSubWin)
+        return alertsData["02-1MOIS-WIN"];
+    else
+        return alertsData["02-1MOIS-LOSE"];
+};
+
+const setAlertSubWithMonth = (newSubData) => {
+    const isSubWin = isWin();
+    const userStreak = getStreakSubMonth(newSubData);
+    sendEventsToAll(getAlertsWithMonth(isSubWin, userStreak));
+};
+
+const getAlertsWithMonth = (isWin, month) => {
+    if (isWin)
+        return alertsData[month + 1];
+    else
+        return alertsData[month];
+};
+
+const getStreakSubMonth = (newSubData) => {
+    if (newSubData.streak_months >= 1 && newSubData.streak_months < 3) {
+        return 0;
+    } else if (newSubData.streak_months >= 3 && newSubData.streak_months < 6) {
+        return 2;
+    } else if (newSubData.streak_months >= 6 && newSubData.streak_months < 9) {
+        return 4;
+    } else if (newSubData.streak_months >= 9 && newSubData.streak_months < 12) {
+        return 6;
+    } else if (newSubData.streak_months >= 12 && newSubData.streak_months < 18) {
+        return 8;
+    } else if (newSubData.streak_months >= 18 && newSubData.streak_months < 24) {
+        return 10;
+    } else if (newSubData.streak_months >= 24) {
+        return 12;
+    }
+};
+
+/* Set Alert Logic */
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -67,14 +83,6 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Credentials', true);
     return next();
 });
-
-const discordCallback = () => {
-    fetch('https://twitch-back.teyz.fr/discordCallback');
-};
-
-const newFollower = (follower) => {
-    client.say('#teyz_', `Bienvenue sur le stream @${follower}`);
-};
 
 function verifySignature(messageSignature, messageID, messageTimestamp, body) {
     let message = messageID + messageTimestamp + body
@@ -96,14 +104,46 @@ app.post("/webhooks/callback", async (req, res) => {
     const { type } = req.body.subscription;
     const { event } = req.body;
 
-    if (type === 'stream.online') {
-        discordCallback();
-    } else if (type === 'channel.follow') {
-        newFollower(event.user_name);
-    }
+    setAlert(event, type);
 
     res.status(200).end();
 });
+
+// ...
+
+function eventsHandler(request, response, next) {
+    const headers = {
+        'Content-Type': 'text/event-stream',
+        'Connection': 'keep-alive',
+        'Cache-Control': 'no-cache'
+    };
+    response.writeHead(200, headers);
+
+    const data = `data: ${JSON.stringify(alertsEventsData)}\n\n`;
+
+    response.write(data);
+
+    const eventId = Date.now();
+
+    const newEvent = {
+        id: eventId,
+        response
+    };
+
+    events.push(newEvent);
+
+    request.on('close', () => {
+        console.log(`${eventId} Connection closed`);
+        events = events.filter(event => event.id !== eventId);
+    });
+}
+
+app.get('/events', eventsHandler);
+
+function sendEventsToAll(newEvent) {
+    alertsEventsData.push({ 'event': newEvent, 'id': alertsEventsData.length });
+    events.forEach(event => event.response.write(`data: ${JSON.stringify({ 'event': newEvent, 'id': alertsEventsData.length })}\n\n`))
+}
 
 const listener = app.listen(port, () => {
     console.log("Your app is listening on port " + listener.address().port);
--- src/token.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  clientSecret: 'ufn4wb5btgfn8ly9nlhcza6ho5dixg',
-  clientId: 's35by58mpigl6jzraszk676rq3l4va',
-  client_name: 'teyz_',
-  password_oauth: 'oauth:yojs5r8m1xfnpa742zz8plqhytcdb4',
-  channels: '#teyz_',
-  bot_username: 'teyz'
-}
